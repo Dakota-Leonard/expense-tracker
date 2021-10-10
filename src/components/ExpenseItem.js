2062e27e8900f78eb79f4b6884174207677124b7
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-10 12:20:31 -0400

    Add dummy data to test styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  //Dummy Data for testing
+  const expenseDate = new Date(2021, 10, 10);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{expenseDate.toDateString()}</div>
       <div>
-        <h2 className="expense-item_description">Title</h2>
-        <div className="expense-item_price">Amount</div>
+        <h2 className="expense-item_description">{expenseTitle}</h2>
+        <div className="expense-item_price">${expenseAmount}</div>
       </div>
     </div>
   );
