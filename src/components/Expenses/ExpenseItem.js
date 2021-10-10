bce76f54d1c49a50b8769d5512fa89ff8aa7dd5f
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-10 19:47:26 -0400

    Remove ExpenseItem title update button and state management

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,12 +5,6 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = props => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle('Updated!');
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
