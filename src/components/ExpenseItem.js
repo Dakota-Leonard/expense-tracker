5257eead2ffbe3489bc4dad8dad4fd9a091c1eaf
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-10 12:12:57 -0400

    Add styling to ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,18 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <>
+    <div className="expense-item">
       <div>Date</div>
       <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+        <h2 className="expense-item_description">Title</h2>
+        <div className="expense-item_price">Amount</div>
       </div>
-    </>
+    </div>
   );
 }
 
+//Date of Purchase
+//Title = Description of purchase
+//Amount = amount spent on purchase
 export default ExpenseItem;
