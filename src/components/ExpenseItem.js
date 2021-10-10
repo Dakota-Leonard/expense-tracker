aa8fae9b57c19cf96c316e48d143a7610b2634a7
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-10 12:31:02 -0400

    Add dummy data to App component to test props styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  //Dummy Data for testing
-  const expenseDate = new Date(2021, 10, 10);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div>
+      <div>{props.date.toDateString()}</div>
       <div>
-        <h2 className="expense-item_description">{expenseTitle}</h2>
-        <div className="expense-item_price">${expenseAmount}</div>
+        <h2 className="expense-item_description">{props.title}</h2>
+        <div className="expense-item_price">${props.amount}</div>
       </div>
     </div>
   );
