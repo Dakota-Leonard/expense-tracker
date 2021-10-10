b76b54caefc2b6c9ad508c0a47e1e5b71bc07d99
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-10 14:30:38 -0400

    Create ExpenseDate component for modularity

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,12 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  //Date Formatting
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-  const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>{month}</div>
-      <div>{day}</div>
-      <div>{year}</div>
-      <div>
-        <h2 className="expense-item_description">{props.title}</h2>
+      <ExpenseDate date={props.date} />
+      <div className="expense-item_description">
+        <h2>{props.title}</h2>
         <div className="expense-item_price">${props.amount}</div>
       </div>
     </div>
