f46fe5fd83b3b147be2c932b6a6d145c1ec2e4e2
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-10 20:17:05 -0400

    Add dynamic rendering of expenses in App and finish new expense adding functionality

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
@@ -9,10 +8,9 @@ const ExpenseItem = props => {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item_description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item_price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
