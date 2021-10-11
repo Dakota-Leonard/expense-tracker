2b9da766a60ddb0d3c42d0f47bc8cd44ea1555d0
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-11 09:10:59 -0400

    Refactor Expenses component filtering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,21 +14,24 @@ const Expenses = props => {
     setFilterYear(filterCriteria);
   };
 
+  //Filter original expense by year selected in dropdown
+  const filterExpenses = expenses.filter(
+    item => item.date.getFullYear().toString() === filterYear
+  );
+
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filterYear} onFilterExpense={onFilterExpense} />
-      {expenses
-        .filter(item => item.date.getFullYear().toString() === filterYear)
-        .map(item => {
-          return (
-            <ExpenseItem
-              key={item.id}
-              title={item.title}
-              amount={item.amount}
-              date={item.date}
-            />
-          );
-        })}
+      {filterExpenses.map(item => {
+        return (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        );
+      })}
     </Card>
   );
 };
