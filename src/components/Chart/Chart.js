7da0d80d108a8aa0a9d6a169f0cb61b309693096
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-12 08:54:24 -0400

    Add max total calculation to Chart component to calculate bar height

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,12 +2,15 @@ import ChartBar from './ChartBar';
 import './ChartBar.css';
 
 const Chart = props => {
+  const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+  const maxTotal = Math.max(...dataPointValues);
+
   return (
     <div className="chart">
       {props.dataPoints.map(dataPoint => (
         <ChartBar
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={maxTotal}
           label={dataPoint.label}
           key={dataPoint.label}
         />
