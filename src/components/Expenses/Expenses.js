be7c2940a92bf14885dfd3abf5827dcb0616c5e1
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-12 08:55:51 -0400

    Add ExpenesChart to Expenses component for rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 const Expenses = props => {
@@ -24,6 +25,7 @@ const Expenses = props => {
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filterYear} onFilterExpense={onFilterExpense} />
+      <ExpensesChart expenes={filterExpenses} />
       <ExpensesList items={filterExpenses} />
     </Card>
   );
